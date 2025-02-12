5f97cf5d7174ca3850fba8b3c4d7c26d8722fee3
unknown <amadodamis@gmail.com>
2025-02-12 11:49:19 -0300

    agregado de users interface, slice, y metodo para llamar los usuarios, transformarlos a Json

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type User struct {
+	ID        uint64 `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+}
 
 func main() {
 	http.HandleFunc("/users", UserServer)
@@ -13,23 +21,54 @@ func main() {
 
 }
 
+var users []User
+
+func init() {
+	users = []User{
+		{ID: 1,
+			FirstName: "Amado",
+			LastName:  "Damis",
+			Email:     "amadodamis@gmail.com",
+		},
+		{ID: 2,
+			FirstName: "Javier",
+			LastName:  "Milei",
+			Email:     "JavierMilei@gmail.com",
+		},
+		{ID: 3,
+			FirstName: "Jair",
+			LastName:  "Bolsonaro",
+			Email:     "JairBolsonaro@gmail.com",
+		},
+	}
+}
+
 func UserServer(w http.ResponseWriter, r *http.Request) {
 	var status int
 
 	switch r.Method {
 	case http.MethodGet:
-		status = 200
-		w.WriteHeader(status)
-		fmt.Fprintf(w, `{"status": %d, "message": "%s"}`, status, "success in get")
+		GetAllUser(w)
 
 	case http.MethodPost:
 		status = 200
 		w.WriteHeader(status)
 		fmt.Fprintf(w, `{"status": %d, "message": "%s"}`, status, "success in post")
-		
+
 	default:
 		status = 404
 		w.WriteHeader(status)
 		fmt.Fprintf(w, `{"status": %d, "message": "%s"}`, status, "not found")
 	}
+}
+
+func GetAllUser(w http.ResponseWriter) {
+	DataResponse(w, http.StatusOK, users)
+}
+
+func DataResponse(w http.ResponseWriter, status int, users interface{}) {
+	value, _ := json.Marshal(users)
+	w.WriteHeader(status)
+	fmt.Fprintf(w, `{"status": %d, "data":%s}`, status, value)
+
 }
