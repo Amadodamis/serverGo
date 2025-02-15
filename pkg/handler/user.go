87c54640bd730c63a0869bf7531fea0e4fcea091
Amado <amadodamis@gmail.com>
2025-02-15 00:08:18 -0300

    method post

--- pkg/handler/user.go
+++ pkg/handler/user.go
@@ -25,18 +25,13 @@ func UserServer(ctx context.Context, endpoints user.EndPoints) func(w http.Respo
 				decodeGetAllUser,
 				encodeResponse,
 				encodeError)
-				return
-		/*case http.MethodPost:
-			decode := json.NewDecoder(r.Body)
-			var req CreateReq
-			if err := decode.Decode(&req); err != nil {
-				MsgResponse(w, http.StatusBadRequest, err.Error())
-				return
-			}
-			PostUser(ctx, s, w, req)
-
-		default:
-			InvalidMethod(w)*/
+			return
+		case http.MethodPost:
+			tran.Server(
+				transport.Endpoint(endpoints.Create),
+				decodeCreateUser,
+				encodeResponse,
+				encodeError)
 		}
 		InvalidMethod(w)
 	}
@@ -46,25 +41,33 @@ func decodeGetAllUser(ctx context.Context, r *http.Request) (interface{}, error)
 	return nil, nil
 }
 
-func encodeResponse(ctx context.Context,w http.ResponseWriter,resp interface{})error{
-	data,err:=json.Marshal(resp)
-	if err!=nil{
-	return err
-	}
-	status:= http.StatusOK
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/-json; charset=utf-8")
-	fmt.Fprintf(w,`{"status" %d, "data":%s}`, status,data)
-	return nil
+func decodeCreateUser(ctx context.Context, r *http.Request) (interface{}, error) {
+	var req user.CreateReq
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		return nil, fmt.Errorf("invalid request format: '%v'", err.Error())
 	}
 
-func encodeError(_ context.Context,err error, w http.ResponseWriter){
-	status:= http.StatusInternalServerError
+	return req, nil
+}
+
+func encodeResponse(ctx context.Context, w http.ResponseWriter, resp interface{}) error {
+	data, err := json.Marshal(resp)
+	if err != nil {
+		return err
+	}
+	status := http.StatusOK
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/-json; charset=utf-8")
+	fmt.Fprintf(w, `{"status" %d, "data":%s}`, status, data)
+	return nil
+}
+
+func encodeError(_ context.Context, err error, w http.ResponseWriter) {
+	status := http.StatusInternalServerError
 	w.WriteHeader(status)
 	w.Header().Set("Content-type", "application/json; charset=utf-8")
 	fmt.Fprintf(w, `{"status": %d, "message":%s}`, status, err.Error())
 }
-
 
 func InvalidMethod(w http.ResponseWriter) {
 	status := http.StatusNotFound
